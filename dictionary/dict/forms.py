68b58a8e3aa886804fc17c690983258239e97f62
katelynk <katelynk@sfu.ca>
2020-09-13 23:32:00 -0700

    Added styling to forms

--- dictionary/dict/forms.py
+++ dictionary/dict/forms.py
@@ -1,7 +1,15 @@
 from django.forms import ModelForm
 from .models import Word
+from django import forms
 
 class WordForm(ModelForm):
     class Meta:
         model = Word
         fields = '__all__'
+
+    widgets = {
+        'term': forms.TextInput(attrs={'class': 'form-control'}),
+        'translated': forms.TextInput(attrs={'class': 'form-control'}),
+        'acronym': forms.Select(attrs={'class': 'form-control'}),
+        'note': forms.Textarea(attrs={'class': 'form-control'}),
+    }
